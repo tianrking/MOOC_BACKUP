e051007740c0e50af00bdf14e6e4daebd627e005
Tienjui Wang <tian.r.king@gmail.com>
2020-03-20 00:32:48 +0800

    daily

---
+++ 数字图像与数字图像处理/matlab_code/BOOK/U2.m
@@ -0,0 +1,27 @@
+% zeros(4,4)
+% ones(4,4)
+% true(4,4)
+% false(4,4)
+% magic(4) %每行每列对角线相等
+% rand(4,4) % 0,1随机
+% randn(4,4) % 正态随机
+% 
+% a=[1,2;3,4]
+% b=[4,3;2,1]
+% c=a.*b
+% d=mat2gray(c)
+% d=a(:)
+% d.'    .' 转置
+%  ctrl R T
+% 
+% figure(10)          % define figure
+% subplot(2,2,1);     % subplot(x,y,n)x表示显示的行数，y表示列数，n表示第几幅图片
+% plot(...);
+% figure(1) plot() figure(2) plot()
+% size(A,1) 维度 行 列 x
+% t= input('massage')
+% disp('A')
+% 第三章
+%imadjust(f,[lowin highin],[lowout highout ],gamma) in 映射到 out
+% 3.3
+
